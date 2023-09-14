706147b3574bbcccddaa3c325cf15292fb14797f
bipo38 <bipoben7@gmail.com>
2023-09-14 20:53:14 +0000

    remove comment

--- internal/isaac/main.go
+++ internal/isaac/main.go
@@ -16,27 +16,3 @@ var globaLink = "https://bindingofisaacrebirth.fandom.com"
 var mainNode = "div.main-container>div.resizable-container>div.has-right-rail>main.page__main>div#content>div#mw-content-text>div.mw-parser-output"
 var TableNode = mainNode + ">table.wikitable>tbody>tr"
 var defaultRoute = "isaac/"
-
-// func GetPaths(category string) []string {
-
-// 	var paths []string
-
-// 	collector := colly.NewCollector()
-
-// 	collector.OnHTML(TableNode, func(h *colly.HTMLElement) {
-// 		path := h.ChildAttr("a", "href")
-
-// 		if path == "" {
-// 			return
-// 		}
-
-// 		print(path)
-
-// 		paths = append(paths, globaLink+path)
-
-// 	})
-
-// 	collector.Visit(globaLink + category)
-
-// 	return paths
-// }
