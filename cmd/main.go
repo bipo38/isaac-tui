b94077fe990fa08b55da2da66c2704d1994b78db
bipo38 <bipoben7@gmail.com>
2023-09-15 16:54:14 +0000

    bosses wip

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,30 @@
 package main
 
-import "isaac-scrapper/internal/isaac"
+import (
+	"fmt"
+
+	"github.com/gocolly/colly"
+)
 
 func main() {
 
 	// isaac.CreateTransformationCsv()
 	// isaac.CreateTrinketsCsv()
 	// isaac.CreateItemsCsv()
+<<<<<<< Updated upstream
 	isaac.CreateCharactersCsv()
+=======
+	// isaac.CreateCharactersCsv()
+	// isaac.CreatePillsCsv()
+	// isaac.CreateBossesCsv()
+
+	collector := colly.NewCollector()
+
+	collector.OnHTML("div.main-container>div.resizable-container>div.has-right-rail>main.page__main>div#content>div#mw-content-text>div.mw-parser-output", func(h *colly.HTMLElement) {
+		fmt.Println(h.ChildAttr("img", "title"))
+	})
+
+	collector.Visit("https://bindingofisaacrebirth.fandom.com/wiki/All_Bosses_(Bosses)#Bosses")
+>>>>>>> Stashed changes
 
 }
