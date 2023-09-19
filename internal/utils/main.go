a1c572a53deb158387778ede8bf336781fdb2dd4
bipo38 <bipoben7@gmail.com>
2023-09-19 17:44:23 +0000

    move to file os functions

--- internal/utils/main.go
+++ internal/utils/main.go
@@ -5,7 +5,6 @@ import (
 	"errors"
 	"fmt"
 	"io"
-	"log"
 	"net/http"
 	"os"
 	"reflect"
@@ -77,54 +76,4 @@ func DownloadImage(url, fPath, fName string) error {
 	}
 
 	return nil
-}
-
-func createDirs(path string) {
-	exist, err := exist(path)
-	if err != nil {
-		log.Fatalf("Failed verify the existence of the folder: %s: %s\n ", path, err)
-	}
-
-	if exist {
-		os.Remove(path)
-	}
-
-	if err := os.MkdirAll(path, os.ModePerm); err != nil {
-		log.Fatalf("Error creating dirs %s: %s\n", path, err)
-	}
-
-	return
-}
-
-func createFile(path string) *os.File {
-
-	exist, err := exist(path)
-	if err != nil {
-		log.Fatalf("Failed verify the existence of the file: %s: %s\n ", path, err)
-	}
-
-	if exist {
-		os.Remove(path)
-	}
-
-	file, err := os.Create(path)
-	if err != nil {
-		log.Fatalf("Error creating file: %s: %s\n ", path, err)
-	}
-
-	return file
-}
-
-func exist(path string) (bool, error) {
-	_, err := os.Stat(path)
-
-	if err == nil {
-		return true, nil
-	}
-
-	if os.IsNotExist(err) {
-		return false, nil
-	}
-
-	return false, err
 }
