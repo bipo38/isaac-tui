fa949fa18017e94361e64c13a37e5982755c5e52
bipo38ls <bipoben7@gmail.com>
2023-09-27 17:58:54 +0200

    fix: forgot to upload

--- config/default.go
+++ config/default.go
@@ -1,8 +1,8 @@
 package config
 
 var Default = map[string]string{
-	"url":                "https://bindingofisaacrebirth.fandom.com",
-	"mainNode":           "div.main-container>div.resizable-container>div.has-right-rail>main.page__main>div#content>div#mw-content-text>div.mw-parser-output",
-	"tableNode":          "div.main-container>div.resizable-container>div.has-right-rail>main.page__main>div#content>div#mw-content-text>div.mw-parser-output>table.wikitable>tbody>tr",
-	"folderDefaultRoute": "isaac/",
+	"url":         "https://bindingofisaacrebirth.fandom.com",
+	"mainNode":    "div.main-container>div.resizable-container>div.has-right-rail>main.page__main>div#content>div#mw-content-text>div.mw-parser-output",
+	"tableNode":   "div.main-container>div.resizable-container>div.has-right-rail>main.page__main>div#content>div#mw-content-text>div.mw-parser-output>table.wikitable>tbody>tr",
+	"folderStart": "isaac",
 }
