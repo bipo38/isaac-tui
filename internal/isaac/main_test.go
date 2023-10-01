e37249ef948f76b23265627b6e3abbb4156602c7
bipo38ls <bipoben7@gmail.com>
2023-10-01 23:01:17 +0200

    feat: add main test

---
+++ internal/isaac/main_test.go
@@ -0,0 +1,53 @@
+package isaac
+
+import "testing"
+
+func TestParseExtension(t *testing.T) {
+
+	cases := []struct {
+		extension string
+		expected  string
+	}{
+		{
+			extension: "Added in Afterbirth †",
+			expected:  "Afterbirth Plus",
+		},
+		{
+			extension: "Afterbirth †",
+			expected:  "Afterbirth Plus",
+		},
+		{
+			extension: "Added in Afterbirth",
+			expected:  "Afterbirth",
+		},
+		{
+			extension: "Afterbirth",
+			expected:  "Afterbirth",
+		},
+		{
+			extension: "Added in Rebirth",
+			expected:  "Rebirth",
+		},
+		{
+			extension: "Rebirth",
+			expected:  "Rebirth",
+		},
+		{
+			extension: "",
+			expected:  "Rebirth",
+		},
+	}
+
+	for _, tt := range cases {
+
+		t.Run("Test Parse Extension", func(t *testing.T) {
+
+			got := parseExtension(tt.extension)
+
+			if got != tt.expected {
+				t.Errorf("got %s, want %s", got, tt.expected)
+			}
+		})
+	}
+
+}
